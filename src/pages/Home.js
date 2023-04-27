ee6000ede5488d7c068c778e533f9e77d806f597
RayChak <raychak@asl.com.hk>
2023-04-28 00:26:34 +0800

    fk

---
+++ src/pages/Home.js
@@ -0,0 +1,61 @@
+import React from 'react'
+import { useEffect } from 'react'
+
+
+export default function Home() {
+  useEffect(()=>{
+        fetch("http://localhost:8080/users").then(
+            response =>{
+                response = response.json();
+                return response;
+            }
+        ).then(data=>{
+            console.log(data)
+        })
+
+
+
+
+
+  },[])
+
+
+
+
+
+  return (
+    <div className='container'>
+        <div className='py-4'>
+        <table className="table border shadow">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">First</th>
+      <th scope="col">Last</th>
+      <th scope="col">Handle</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Jacob</td>
+      <td>Thornton</td>
+      <td>@fat</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td colspan="2">Larry the Bird</td>
+      <td>@twitter</td>
+    </tr>
+  </tbody>
+</table>
+        </div>
+    </div>
+  )
+}
