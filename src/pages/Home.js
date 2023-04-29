4d641a48cc4efec495cf9adb7ecbe126f29191b2
RayChak <raychak@asl.com.hk>
2023-04-29 23:23:18 +0800

    okok

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,60 +1,67 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useState } from 'react'
 import { useEffect } from 'react'
 
 
 export default function Home() {
+  const [user,setUser] = useState([]);
   useEffect(()=>{
-        fetch("http://localhost:8080/users").then(
-            response =>{
-                response = response.json();
-                return response;
-            }
-        ).then(data=>{
-            console.log(data)
-        })
-
-
-
-
-
+    loadusers();
   },[])
 
-
+  const loadusers = async () =>{
+    const result = await axios.get("http://localhost:8080/users");
+    
+    setUser(result.data);
+    console.log(user);
+    
+  }
 
 
 
   return (
     <div className='container'>
         <div className='py-4'>
-        <table className="table border shadow">
-  <thead>
-    <tr>
-      <th scope="col">#</th>
-      <th scope="col">First</th>
-      <th scope="col">Last</th>
-      <th scope="col">Handle</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <th scope="row">1</th>
-      <td>Mark</td>
-      <td>Otto</td>
-      <td>@mdo</td>
-    </tr>
-    <tr>
-      <th scope="row">2</th>
-      <td>Jacob</td>
-      <td>Thornton</td>
-      <td>@fat</td>
-    </tr>
-    <tr>
-      <th scope="row">3</th>
-      <td colspan="2">Larry the Bird</td>
-      <td>@twitter</td>
-    </tr>
-  </tbody>
-</table>
+            <table className="table border shadow">
+                <thead>
+                  <tr>
+                    <th scope="col">ID</th>
+                    <th scope="col">Name</th>
+                    <th scope="col">User Name</th>
+                    <th scope="col">Email</th>
+                    <th scope="col">Edit</th>
+                  </tr>
+                </thead>
+                <tbody>
+
+                  {
+                    user.map((user,index)=>{
+                      return( 
+                      <tr>
+                        <th scope="row" key={index}>{index+1}</th>
+                        <td>{user.name}</td>
+                        <td>{user.username}</td>
+                        <td>{user.email}</td>
+                        <td><button type="button" class="btn btn-primary mx-2">View</button>
+                        <button type="button" class="btn btn-outline-primary mx-2">Edit</button>
+                        <button type="button" class="btn btn-danger mx-2">Delete</button>
+                        </td>
+
+                      </tr>
+                      )
+                     
+                    })
+                  }
+
+
+                  {/* <tr>
+                    <th scope="row">1</th>
+                    <td>Mark</td>
+                    <td>Mark</td>
+                    <td>Mark</td>
+                  </tr>   */}
+                </tbody>
+            </table>
         </div>
     </div>
   )
